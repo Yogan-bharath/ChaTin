89ad5a168f4e775375e46dd0596c3c604dd3ad99
Yogan-bharath <yoganbharath@gmail.com>
2026-04-22 21:44:31 +0530

    Added server files

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-const http = require('http')
-const express = require('express')
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const { Server } = require('socket.io');
+require('dotenv').config();
+
 const app = express();
-const server = http.createServer(app)
-const path = require('path')
-const {Server} = require('socket.io')
-const io = new Server(server)
-require('dotenv').config()
-const PORT = process.env.PORT || 3000
-app.get('/public',(req,res)=>{
-    res.sendFile(path.join(__dirname,"html/index.html"))
-})
+const server = http.createServer(app);
 
-io.on('connection',(socket)=>{
-    socket.on('user-message',(message)=>{
-        io.emit("message",message);
-    })
-})
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT,()=>{
-    console.log("srever is running in ",PORT);
-})
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "html/index.html"));
+});
+
+io.on('connection', (socket) => {
+  socket.on('user-message', (message) => {
+    io.emit("message", message);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("Server is running on", PORT);
+});
